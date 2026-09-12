76c6e02262678a34105944582570573020b0c59d
Ebenezer Kwaw <ekwaw4545@gmail.com>
2026-09-12 11:06:27 +0000

    Connect app to Clinicore Firebase project

--- Frontend/firebase-config.js
+++ Frontend/firebase-config.js
@@ -1,11 +1,13 @@
-// Replace the object values with your Firebase project's web app config
+// Firebase web configuration for Clinicore Intelligence
+// These identifiers are designed to be used by the browser client.
 const FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD7W5yP1-4YPXFfqIaNyEDej-LVTfysZyY",
+  authDomain: "clinicore-intelligence.firebaseapp.com",
+  projectId: "clinicore-intelligence",
+  storageBucket: "clinicore-intelligence.firebasestorage.app",
+  messagingSenderId: "608463898292",
+  appId: "1:608463898292:web:99b167930b221a5fdc39e1",
+  measurementId: "G-EJDSB4BX8P"
 };
 
 export default FIREBASE_CONFIG;
